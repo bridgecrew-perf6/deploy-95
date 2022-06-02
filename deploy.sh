dbff121bdb68202fec470335329754282d484887
ben <boneill@rnet-tech.com>
2022-06-02 09:46:17 -0400

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 docker build -t vnv_serve_it --build-arg CONFIG_FILE=$1 .
-docker run --network="host" -v /var/run/docker.sock:/var/run/docker.sock vnv_serve_it
+docker run -td  --network="host" -v /var/run/docker.sock:/var/run/docker.sock vnv_serve_it

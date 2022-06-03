479abc2efe39916fef35a7bdb82c98607f3fe7a8
ben <boneill@rnet-tech.com>
2022-06-03 08:54:21 -0400

    update deploy script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # deploy
 Small Deployment template that shows how to use your own config file when deploying the saas model.
+
+
+Usage:
+
+   ./deploy.sh <tag> <config-file>
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-docker build -t vnv_serve_it --build-arg CONFIG_FILE=$1 .
-docker run -td  --network="host" -v /var/run/docker.sock:/var/run/docker.sock vnv_serve_it
+docker pull ghcr.io/vnvlabs/serve:$1
+docker build -t vnv_serve_it --build-arg FROM_IMAGE=ghcr.io/vnvlabs/serve:$1 --build-arg CONFIG_FILE=$2 .
+docker run -td --name=vnvlabs --network="host" -v /var/run/docker.sock:/var/run/docker.sock vnv_serve_it
+docker logs --follow vnvlabs
+

a8e2e3e87560b5eb8191a9293f4f937f61f0bdae
ben <boneill@rnet-tech.com>
2022-06-03 09:49:58 -0400

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 docker pull ghcr.io/vnvlabs/serve:$1
 docker build -t vnv_serve_it --build-arg FROM_IMAGE=ghcr.io/vnvlabs/serve:$1 --build-arg CONFIG_FILE=$2 .
+
+docker container kill vnvlabs
+docker container rm vnvlabs
+
 docker run -td --name=vnvlabs --network="host" -v /var/run/docker.sock:/var/run/docker.sock vnv_serve_it
 docker logs --follow vnvlabs
 
